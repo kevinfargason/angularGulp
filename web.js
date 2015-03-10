727d06578f5a4679bdea931f227dcf4dba688f36
kevin <kfargason@wm.com>
2015-03-10 17:30:09 -0500

    used node express server in web.js

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
-var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("/dist"));
-app.listen(process.env.PORT || 5000);
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/dist/', function(request, response) {
+  // response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});

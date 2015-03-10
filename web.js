eea6db2fa3a33320b33def8dce78525febb720c6
kevin <kfargason@wm.com>
2015-03-10 17:54:13 -0500

    trying to run on heroku with gulp run in Procfile

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/dist/', function(request, response) {
-  // response.send('Hello World!');
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
